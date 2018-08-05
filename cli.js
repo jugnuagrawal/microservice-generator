b3c9a804e6db12f14102f302147c205125bc0a85
Jugnu Agrawal <jugnuagrawal.brjn@gmail.com>
2018-08-05 17:49:27 +0530

    cli global install

--- cli.js
+++ cli.js
@@ -10,12 +10,12 @@ const rl = readline.createInterface({
 });
 
 function ask() {
-    rl.question('Enter the path of your schema file [default: sampleSchema.json] : ', (_fileName) => {
-        if(!_fileName.trim()){
+    rl.question('Enter the full path of your schema file [sampleSchema.json] : ', (_fileName) => {
+        if (!_fileName.trim()) {
             console.log('No file name entered, using sampleSchema.json');
             rl.close();
-            getSchema('sampleSchema.json');            
-        }else if (_fileName.match(/^[\w\s\\\/]+\.json$/)) {
+            getSchema('sampleSchema.json');
+        } else if (_fileName.match(/^.*\.json$/i)) {
             getSchema(_fileName);
             rl.close();
         } else {
@@ -34,7 +34,9 @@ function getSchema(_fileName) {
     if (fs.existsSync(_fileName)) {
         fs.readFile(_fileName, 'utf8', (_err, _data) => {
             if (_err) throw _err;
-            generator.createProject(JSON.parse(_data));
+            const json = JSON.parse(_data);
+            json.filePath = _fileName;
+            generator.createProject(json);
         });
     } else {
         console.log(_fileName, ' does not exist.');
--- index.js
+++ index.js
@@ -15,7 +15,13 @@ String.prototype.toCamelCase = function () {
 function createProject(_data) {
     var _name = _data.name.toCamelCase();
     var _api = _data.api ? _data.api : _name;
-    var _path = path.join('../', _name);
+    var _path = path.join(_name);
+    if (_data.filePath) {
+        var segments = _data.filePath.split('/');
+        segments.pop();
+        segments.push(_name);
+        _path = segments.join('/');
+    }
     var _database = _data.database ? _data.database : _name;
     var _port = _data.port ? _data.port : 3000;
     if (!fs.existsSync(_path)) {
@@ -36,10 +42,10 @@ function createProject(_data) {
     if (!fs.existsSync(path.join(_path, 'apidoc'))) {
         fs.mkdirSync(path.join(_path, 'apidoc'));
     }
-    
+
     fs.writeFileSync(path.join(_path, 'controllers', _name + '.controller.js'), controller.getContent(_name, _data.id), 'utf-8')
     console.log(_name + '.controller.js created!');
-    fs.writeFileSync(path.join(_path, 'schemas', _name + '.schema.json'), JSON.stringify(_data.schema,null,4), 'utf-8')
+    fs.writeFileSync(path.join(_path, 'schemas', _name + '.schema.json'), JSON.stringify(_data.schema, null, 4), 'utf-8')
     console.log(_name + '.schema.json created!');
     fs.copyFileSync(path.join(__dirname, 'templates/messages.template.json'), path.join(_path, 'messages', _name + '.messages.json'));
     console.log(_name + '.messages.json created!');
