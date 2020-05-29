2e97c68aff7ff10b644c398ed7a6d50836c58cd3
Jugnu Agrawal <jugnu@firefly.local>
2020-05-29 22:44:21 +0530

    filter fix

--- templates/utils.js
+++ templates/utils.js
@@ -1,11 +1,57 @@
 const _ = require('lodash');
 
+IsString = (val) => val && val.constructor.name === 'String';
+IsArray = (arg) => arg && arg.constructor.name === 'Array';
+IsObject = (arg) => arg && arg.constructor.name === 'Object';
+function CreateRegexp(str) {
+    if (str.charAt(0) === '/' && str.charAt(str.length - 1) === '/') {
+        var text = str.substr(1, str.length - 2).replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+        return new RegExp(text, 'i');
+    } else {
+        return str;
+    }
+}
+function ResolveArray(arr) {
+    for (var x = 0; x < arr.length; x++) {
+        if (IsObject(arr[x])) {
+            arr[x] = FilterParse(arr[x]);
+        } else if (IsArray(arr[x])) {
+            arr[x] = ResolveArray(arr[x]);
+        } else if (IsString(arr[x])) {
+            arr[x] = CreateRegexp(arr[x]);
+        }
+    }
+    return arr;
+}
+function FilterParse(filterParsed) {
+    for (var key in filterParsed) {
+        if (IsString(filterParsed[key])) {
+            filterParsed[key] = CreateRegexp(filterParsed[key])
+        } else if (IsArray(filterParsed[key])) {
+            filterParsed[key] = ResolveArray(filterParsed[key])
+        } else if (IsObject(filterParsed[key])) {
+            filterParsed[key] = FilterParse(filterParsed[key])
+        }
+    }
+    return filterParsed
+}
+
 /**
  * 
  * @param {object} filter 
  */
 function parseFilter(filter) {
-    return filter;
+    let tempFilter = filter;
+    try {
+        if (typeof filter === 'string') {
+            tempFilter = JSON.parse(filter);
+        }
+        tempFilter = FilterParse(tempFilter);
+    } catch (e) {
+        tempFilter = FilterParse(tempFilter);
+    } finally {
+        return tempFilter;
+    }
 }
 
 
